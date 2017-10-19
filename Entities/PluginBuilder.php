248b053b4c58081f11fa7e3c4d1aa2282f6717a9
badi-zixdev <badi1wow@gmail.com>
2017-10-19 10:14:39 +0100

    init

---
+++ Entities/PluginBuilder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Zix\PluginBuilder\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class PluginBuilder
+ * @package Zix\PluginBuilder\Entities
+ */
+class PluginBuilder extends Model
+{
+    /**
+     * @var array
+     */
+    protected $fillable = [];
+}
---
+++ Providers/PluginBuilderServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Zix\PluginBuilder\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class PluginBuilderServiceProvider extends ServiceProvider {
+
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		//
+	}
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+}
