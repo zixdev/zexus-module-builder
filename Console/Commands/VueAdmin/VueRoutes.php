0674fd7a6da368a839b6c6761ee99c3068569fe2
badi-zixdev <badi1wow@gmail.com>
2017-10-24 12:37:09 +0100

    updated: vue route generator

--- Console/Commands/VueAdmin/VueRoutes.php
+++ Console/Commands/VueAdmin/VueRoutes.php
@@ -10,46 +10,68 @@ class VueRoutes extends GeneratorCommand
 {
     use StubGeneratorTrait;
 
-	/**
-	 * The name and signature of the console command.
-	 *
-	 * @var string
-	 */
-	protected $signature = 'zix:va-route {name : The Command Name}
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'zix:va-route {name : The Command Name}
 										{module : The Module Name}';
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Command description.';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description.';
 
 
-	/**
-	 * Execute the console command.
-	 *
-	 * @return mixed
-	 */
-	public function handle()
-	{
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
         $this->generatePath = 'Assets/admin/js/routes';
         $this->stubPath = 'plugins/PluginBuilder/Console/Commands/stubs/vue-admin/route.stub';
 
         parent::handle();
-	}
+    }
 
     /**
      * Get the destination class path.
      *
-     * @param  string  $name
+     * @param  string $name
      * @return string
      */
     protected function getPath($name)
     {
         $name = Str::replaceFirst($this->rootNamespace(), '', $name);
+        return base_path('plugins/' . $this->argument('module') . '/Assets/admin/js/routes/' . $name . '.js');
+    }
 
-        return $this->laravel['path'].'/'.str_replace('\\', '/', $name).'.js';
+    /**
+     * Replace the namespace for the given stub.
+     *
+     * @param  string  $stub
+     * @param  string  $name
+     * @return $this
+     */
+    protected function replaceNamespace(&$stub, $name)
+    {
+        $stub = str_replace(
+            ['Path', 'Name', 'Component', 'Permission'],
+            [
+                $this->argument('name'),
+                $this->argument('name'),
+                $this->argument('name'),
+                $this->argument('name'),
+            ],
+            $stub
+        );
+
+        return $this;
     }
 
 
