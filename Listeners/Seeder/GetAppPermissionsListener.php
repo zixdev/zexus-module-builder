0902628caf0d75313eae70fd2a8fec57ab9f1d7c
badi-zixdev <badi1wow@gmail.com>
2018-01-15 17:25:43 +0100

    updated: permission listener

--- Listeners/Seeder/GetAppPermissionsListener.php
+++ Listeners/Seeder/GetAppPermissionsListener.php
@@ -7,7 +7,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Zix\Core\Events\Seeder\GetAppPermissions;
 
-class AppPermissionsCreateListener
+class GetAppPermissionsListener
 {
     /**
      * Handle the event.
--- Providers/EventServiceProvider.php
+++ Providers/EventServiceProvider.php
@@ -4,7 +4,7 @@ namespace Zix\PluginBuilder\Providers;
 
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Zix\Core\Events\Seeder\GetAppPermissions;
-use Zix\PluginBuilder\Listeners\Seeder\AppPermissionsCreateListener;
+use Zix\PluginBuilder\Listeners\Seeder\GetAppPermissionsListener;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -15,7 +15,7 @@ class EventServiceProvider extends ServiceProvider
      */
     protected $listen = [
         GetAppPermissions::class => [
-            AppPermissionsCreateListener::class,
+            GetAppPermissionsListener::class,
         ]
     ];
 
